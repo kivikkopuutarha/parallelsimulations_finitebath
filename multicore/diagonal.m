df97f76a139d08596812c04466e5e51a2455c9b9
Tsiormpatzis Stergios <stergios.tsiormpatzis@aalto.fi>
2024-07-14 00:54:05 +0300

    Upload multicore diagonal File

---
+++ multicore/diagonal.m
@@ -0,0 +1,14 @@
+% GPU VERSION
+% A function that diagonalize the total hamiltonian
+% vel:  the unitary matrix whose columns are the eigenvectors of H
+% el:   the diagonal matrix of the eigenvalues (energies) of H
+
+function [vel, el] = diagonal (H)
+
+[vel, el] = eig(H);
+
+% UNCOMMENT if you need to gather the results from the GPU
+%vel = gather(vel);
+%el = gather(el);
+
+end
