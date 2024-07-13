9fa33cbfc3ec173667391d91e8d77da863193721
Tsiormpatzis Stergios <stergios.tsiormpatzis@aalto.fi>
2024-07-14 00:50:52 +0300

    Upload modular diagonal File

---
+++ modular/diagonal.m
@@ -0,0 +1,9 @@
+% A function that diagonalize the total hamiltonian
+% vel:  the unitary matrix whose columns are the eigenvectors of H
+% el:   the diagonal matrix of the eigenvalues (energies) of H
+
+function [vel, el] = diagonal (H)
+
+[vel, el] = eig(H);
+
+end
