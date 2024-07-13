0fc3e71ce4e95a44d776971514fb39851cdbdfeb
Tsiormpatzis Stergios <stergios.tsiormpatzis@aalto.fi>
2024-07-14 00:54:28 +0300

    Upload multicore GGE File

---
+++ multicore/GGE.m
@@ -0,0 +1,26 @@
+% Calculate the numerical GGE prediction for the populations,
+% which is to be compared with the long-time evolution.
+% Basically, we just implement a convolution formula.
+% 
+% Input variables:
+% N:    The total number of two level systems (TLSs) in the bath.
+%       The intially excited state, the qubit, is not considered to be
+%       part of the bath. Therefore N+1 is the overall number of TLSs
+% vel:  a matrix with column eigenvectors (from total_hamiltonian)
+% 
+% Output
+% nau:  the result of GGE
+
+function nau = GGE (N, vel)
+
+nau = distributed.zeros(1, N+1);
+ujt = abs(vel(N+1,:)).^2;
+
+for k = 1:(N+1)
+    uki = abs(vel(k,:)).^2;
+    nau(k) = dot(ujt, uki);
+end
+
+nau = gather(nau);
+
+end
