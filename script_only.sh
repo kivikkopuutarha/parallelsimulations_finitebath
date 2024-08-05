51d223ae52a14b52985ac6ba86b757aa7a1779f6
Tsiormpatzis Stergios <stergios.tsiormpatzis@aalto.fi>
2024-08-05 09:40:52 +0300

    Delete script_only.sh

--- script_only.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=02:00:00
-#SBATCH --mem=128G
-#SBATCH --output=simulation.out
-#SBATCH --cpus-per-task=40
-
-module load matlab
-
-srun matlab -nodisplay -nosplash -batch run_all
