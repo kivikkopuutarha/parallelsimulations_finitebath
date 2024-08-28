027db05874a6f1d23c1e5e011124a4151234fd41
Tsiormpatzis Stergios <stergios.tsiormpatzis@aalto.fi>
2024-08-28 10:31:14 +0300

    Update diagonal.m

--- src/modular/diagonal.m
+++ src/modular/diagonal.m
@@ -1,3 +1,5 @@
+% MODULAR VERSION
+
 % A function that diagonalize the total hamiltonian
 % vel:  the unitary matrix whose columns are the eigenvectors of H
 % el:   the diagonal matrix of the eigenvalues (energies) of H
